19faebff163d5315236be7470fae23af6a8f0b51
Ujjwaleshwar Srikanth <banndband@gmail.com>
2021-09-10 21:43:50 +0530

    Added song info form to frontend

--- frontend/src/SongInfo.js
+++ frontend/src/SongInfo.js
@@ -1,20 +1,89 @@
+import axios from 'axios';
+import update from 'immutability-helper';
 import React, { Fragment } from 'react';
 
 class SongInfo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            uname: null,
+            namelist: [],
+            solosong: {
+                songname: '',
+                artist: ''
+            },
+            duetsong: [{songname: '', artist: '', type: 'same', preference: 0}, {songname: '', artist: '', type: 'same', preference: 0}]
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSoloChange = this.handleSoloChange.bind(this);
+        this.handleDuetChange = this.handleDuetChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount()
+    {
+        axios.get('http://localhost:8080/participant')
+        .then(response => {
+            console.log(response);
+            this.setState({namelist: response.data});
+        })
+    }
+
+    handleSoloChange(name, value){
+        let newState = update(this.state, {
+            solosong: {
+                [name]: {$set: value}
+            }
+        });
+        this.setState(newState);
+    }
+
+    handleDuetChange(index, name, value){
+        let newState = update(this.state, {
+            duetsong: {
+                [index]: {
+                    [name]: {$set: value}
+                }
+            }
+        });
+        this.setState(newState);
+    }
+
+    handleChange(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
+        const namelist = this.state.namelist;
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
+                Select your name: 
+                <select name="uname" id="uname" onChange={this.handleChange}>
+                    <option value={null}>Select a name</option>
+                    {namelist.map((option) => (
+                        <option value={option.mobileno}>{option.firstname + " " + option.lastname}</option>
+                    ))}
+                </select>
+                <br />
                 Solo Song:
                 <br />
-                <Solo />
+                <Solo parentCallback={this.handleSoloChange}/>
                 <br />
                 Duet Song 1:
                 <br />
-                <Duet partList={list}/>
+                <Duet index={0} partList={this.state.namelist} parentCallback={this.handleDuetChange}/>
                 <br />
                 Duet Song 2:
                 <br />
-                <Duet partList={list}/>
+                <Duet index={1} partList={this.state.namelist} parentCallback={this.handleDuetChange}/>
                 <br />
                 <input type="submit" />
             </form>
@@ -22,51 +91,66 @@ class SongInfo extends React.Component{
     }
 }
 
-function Solo(props){
-    return(
-        <Fragment>
-            <label htmlFor="soloname">Song Name: </label>
-            <input type="text" id="soloname" name="soloname" />
-            <label htmlFor="soloartistname">Artist Name: </label>
-            <input type="text" id="soloartistname" name="soloartistname" />
-        </Fragment>  
-    );
-}
+class Solo extends React.Component{
+    constructor(props){
+        super(props);
 
-function Duet(props){
-    const list = props.partList;
-    return(
-        <Fragment>
-            <label htmlFor="duetname">Song Name: </label>
-            <input type="text" id="duetname" name="duetname" />
-            <label htmlFor="duet1artistname">Artist Name: </label>
-            <input type="text" id="artistname" name="artistname" />
-            <laber htmlFor="type">Select Duet Type: </laber>
-            <select name="type" id="type">
-                <option value="same">Same Gender</option>
-                <option value="mixed">Mixed Gender</option>
-            </select>
-            <laber htmlFor="preference">Select Preferred Partner: </laber>
-            <select name="preference" id="preference">
-                {list.map((option) => (
-                    <option value={option.mobileno}>{option.firstname + " " + option.lastname}</option>
-                ))}
-            </select>
-        </Fragment>
-    );
-}
-
-const list = [
-    {
-        firstname: "Abcd",
-        lastname: "Efgh",
-        mobileno: 1234567890
-    },
-    {
-        firstname: "Bcd",
-        lastname: "Fgh",
-        mobileno: 1023456789
+        this.handleChange = this.handleChange.bind(this);
     }
-];
+    handleChange(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        this.props.parentCallback(name, value);
+    }
+    render(){
+        return(
+            <Fragment>
+                <label htmlFor="songname">Song Name: </label>
+                <input type="text" id="songname" name="songname" onChange={this.handleChange}/>
+                <label htmlFor="artist">Artist Name: </label>
+                <input type="text" id="artist" name="artist" onChange={this.handleChange}/>
+            </Fragment>  
+        );
+    }
+    
+}
+
+class Duet extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        const index = this.props.index;
+        this.props.parentCallback(index, name, value);
+    }
+    render(){
+        const list = this.props.partList;
+        return(
+            <Fragment>
+                <label htmlFor="songname">Song Name: </label>
+                <input type="text" id="songname" name="songname" onChange={this.handleChange}/>
+                <label htmlFor="artist">Artist Name: </label>
+                <input type="text" id="artist" name="artist" onChange={this.handleChange}/>
+                <label htmlFor="type">Select Duet Type: </label>
+                <select name="type" id="type" onChange={this.handleChange}>
+                    <option value="same">Same Gender</option>
+                    <option value="mixed">Mixed Gender</option>
+                </select>
+                <label htmlFor="preference">Select Preferred Partner: </label>
+                <select name="preference" id="preference" onChange={this.handleChange}>
+                    <option value={null}>Select a name</option>
+                    {list.map((option) => (
+                        <option value={option.mobileno}>{option.firstname + " " + option.lastname}</option>
+                    ))}
+                </select>
+            </Fragment>
+        );
+    }
+}
+
 
 export default SongInfo;
