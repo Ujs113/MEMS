7f9396f1759f81eb3fd5864c2713ac92e30c06e3
Ujjwaleshwar Srikanth <banndband@gmail.com>
2021-10-16 20:15:01 +0530

    Added patch route for event requests

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,5 @@
 const express = require('express');
-// const http = require('http');
-// const WebSocket = require('ws');
 const app = express();
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -11,7 +7,7 @@ app.use(express.json());
 const eventRoutes = require('./routes/event.js');
 const participantRoutes = require('./routes/participant.js');
 const songRoutes = require('./routes/songs');
-const Participant = require('./models/Participant.js');
+
 
 
 var corsOptions = {
@@ -22,18 +18,6 @@ app.use(cors(corsOptions));
 
 //connect to db
 mongoose.connect(process.env.CONNECTION_STRING, () => console.log('connected to db'));
-
-//server.on('request', app);
-
-//websocket
-// wss.on('connection', async (ws) => {
-//   var data = await Participant.find();
-//   ws.send(JSON.stringify(data));
-//   Participant.watch({fullDocument: 'updateLookup'}).on('change', (data) => {
-//     ws.send(JSON.stringify(data));
-//     console.log(data);
-//   });
-// })
 
 //routes
 app.use('/event', eventRoutes);
--- backend/routes/event.js
+++ backend/routes/event.js
@@ -3,10 +3,27 @@ const router = express.Router();
 const Event = require('../models/Event');
 
 router.get('/', async (req,res) => {
-    console.log('request received');
     const status = await Event.find();
     res.status(200).send(status);
 });
+
+router.patch('/', async (req, res) => {
+
+    const update = {
+        isOrganized: req.body.isOrganized,
+        partInfoCollected: req.body.partInfoCollected,
+        songInfoCollected: req.body.songInfoCollected
+    }
+
+    try{
+        const ret = await Event.findOneAndUpdate({_id: req.body._id}, update, {new:true});
+        res.status(200).send(ret);
+    }catch(err){
+        res.status(400).json({
+            message: err
+        })
+    }
+})
 
 router.post('/', async (req, res) => {
     const event = new Event({
