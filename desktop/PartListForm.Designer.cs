e4ed252bf72924b95bb0b9c0662ae2f70dabf55a
Ujjwaleshwar Srikanth <banndband@gmail.com>
2021-10-17 21:21:21 +0530

    MEMS-19 Added add functionality to participant list form

--- desktop/PartListForm.Designer.cs
+++ desktop/PartListForm.Designer.cs
@@ -34,12 +34,19 @@ namespace Music_event_management_system
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.firstnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lastnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mobilenoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.duetSizeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.participantBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.participantBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -59,22 +66,22 @@ namespace Music_event_management_system
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(659, 105);
+            this.button1.Location = new System.Drawing.Point(627, 162);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(257, 73);
+            this.button1.Size = new System.Drawing.Size(195, 53);
             this.button1.TabIndex = 1;
-            this.button1.Text = "EDIT";
+            this.button1.Text = "Update";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(659, 250);
+            this.button2.Location = new System.Drawing.Point(639, 381);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(257, 73);
+            this.button2.Size = new System.Drawing.Size(195, 69);
             this.button2.TabIndex = 2;
             this.button2.Text = "FINALIZE";
             this.button2.UseVisualStyleBackColor = true;
@@ -95,6 +102,42 @@ namespace Music_event_management_system
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(542, 344);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(886, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(886, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(886, 195);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(886, 242);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(886, 287);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(148, 20);
+            this.textBox5.TabIndex = 8;
             // 
             // firstnameDataGridViewTextBoxColumn
             // 
@@ -129,13 +172,44 @@ namespace Music_event_management_system
             // participantBindingSource
             // 
             this.participantBindingSource.DataSource = typeof(Music_event_management_system.Participant);
+            this.participantBindingSource.ListChanged += new System.ComponentModel.ListChangedEventHandler(this.participantBindingSource_ListChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(627, 82);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 53);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(627, 242);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(195, 53);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // PartListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(938, 473);
+            this.ClientSize = new System.Drawing.Size(1135, 463);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -163,5 +237,12 @@ namespace Music_event_management_system
         private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn mobilenoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn duetSizeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- desktop/PartListForm.cs
+++ desktop/PartListForm.cs
@@ -27,10 +27,33 @@ namespace Music_event_management_system
             _httpClient = httpClient;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Successfully Edited");
+            var part = new Participant
+            {
+                firstname = textBox1.Text,
+                lastname = textBox2.Text,
+                gender = textBox3.Text,
+                mobileno = long.Parse(textBox4.Text),
+                duetSize = int.Parse(textBox5.Text)
+            };
+            await addParticipant(part);
+            var res = await utility.getparticipants(_httpClient);
+            var list = new BindingList<Participant>(res);
+            source = new BindingSource(list, null);
+            dataGridView1.DataSource = source;
+            MessageBox.Show("Successfully Updated");
+        }
 
+        private async Task<bool> updateParticipant(Participant part)
+        {
+            var options = new JsonSerializerOptions();
+            string jsonString = JsonSerializer.Serialize(part, options);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var req = new HttpRequestMessage(new HttpMethod("PATCH"), $"/participant");
+            req.Content = content;
+            var res = await _httpClient.SendAsync(req);
+            return res.IsSuccessStatusCode;
         }
 
         private async void button2_Click(object sender, EventArgs e)
@@ -53,27 +76,7 @@ namespace Music_event_management_system
             {
                 MessageBox.Show("Participant list already finalized!");
             }
-            //MessageBox.Show("Finalized");
         }
-
-        //private async Task<EventState> getEventState()
-        //{
-        //    var response = await _httpClient.GetFromJsonAsync<List<EventState>>($"/event");
-        //    return response[0];
-        //}
-
-        //private async Task<bool> updateEventState(EventState e)
-        //{
-        //    var options = new JsonSerializerOptions();
-        //    string jsonString = JsonSerializer.Serialize(e, options);
-        //    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-        //    var req = new HttpRequestMessage(new HttpMethod("PATCH"), $"/event")
-        //    {
-        //        Content = content
-        //    };
-        //    var res = await _httpClient.SendAsync(req);
-        //    return res.IsSuccessStatusCode;
-        //}
 
         private async void Form4_Load(object sender, EventArgs e)
         {
@@ -81,21 +84,56 @@ namespace Music_event_management_system
             var list = new BindingList<Participant>(res);
             source = new BindingSource(list, null);
             dataGridView1.DataSource = source;
-        }
-        //private async Task<List<Participant>> getparticipants()
-        //{
-        //    var response = await _httpClient.GetFromJsonAsync<List<Participant>>($"/participant/populated");
-        //    return response;   
-        //}
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
+        }    
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var index = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[index];
+            if(row.Cells[0].Value != null)
+            {
+                textBox1.Text = row.Cells[0].Value.ToString();
+                textBox2.Text = row.Cells[1].Value.ToString();
+                textBox3.Text = row.Cells[2].Value.ToString();
+                textBox4.Text = row.Cells[3].Value.ToString();
+                textBox5.Text = row.Cells[4].Value.ToString();
+            }
+            
+        }
+
+        private void participantBindingSource_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            var changedType = e.ListChangedType;
+            MessageBox.Show(changedType.ToString());
+        }
+
+        private async Task<bool> addParticipant(Participant part)
+        {
+            var res = await _httpClient.PostAsJsonAsync($"/participant", part);
+            return res.IsSuccessStatusCode;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var part = new Participant
+            {
+                firstname = textBox1.Text,
+                lastname = textBox2.Text,
+                gender = textBox3.Text,
+                mobileno = long.Parse(textBox4.Text),
+                duetSize = int.Parse(textBox5.Text)
+            };
+            await addParticipant(part);
+            var res = await utility.getparticipants(_httpClient);
+            var list = new BindingList<Participant>(res);
+            source = new BindingSource(list, null);
+            dataGridView1.DataSource = source;
+            MessageBox.Show("Successfully Added");
         }
     }
 }
